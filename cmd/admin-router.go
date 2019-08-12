0d97cb56c555b6d373ac53e190406c90cf37d3f0
sinhaashish <ashi.sinha.87@gmail.com>
2019-08-12 15:44:28 +0530

    Add method to fetch Vault info

--- cmd/admin-router.go
+++ cmd/admin-router.go
@@ -50,8 +50,11 @@ func registerAdminRouter(router *mux.Router, enableConfigOps, enableIAMOps bool)
 	// Service restart and stop - TODO
 	adminV1Router.Methods(http.MethodPost).Path("/service").HandlerFunc(httpTraceAll(adminAPI.ServiceStopNRestartHandler))
 
-	// Info operations
-	adminV1Router.Methods(http.MethodGet).Path("/info").HandlerFunc(httpTraceAll(adminAPI.ServerInfoHandler))
+	// Info operations - return details based on input type
+	adminV1Router.Methods(http.MethodGet).Path("/info").HandlerFunc(httpTraceAll(adminAPI.ServerInfoHandler)).Queries("infoType", "{infoType:.*}")
+
+	// // Vault Info operations
+	// adminV1Router.Methods(http.MethodGet).Path("/vault").HandlerFunc(httpTraceAll(adminAPI.VaultInfoHandler))
 
 	if globalIsDistXL || globalIsXL {
 		/// Heal operations
@@ -66,9 +69,6 @@ func registerAdminRouter(router *mux.Router, enableConfigOps, enableIAMOps bool)
 		/// Health operations
 
 	}
-	// Performance command - return performance details based on input type
-	adminV1Router.Methods(http.MethodGet).Path("/performance").HandlerFunc(httpTraceAll(adminAPI.PerfInfoHandler)).Queries("perfType", "{perfType:.*}")
-
 	// Profiling operations
 	adminV1Router.Methods(http.MethodPost).Path("/profiling/start").HandlerFunc(httpTraceAll(adminAPI.StartProfilingHandler)).
 		Queries("profilerType", "{profilerType:.*}")
